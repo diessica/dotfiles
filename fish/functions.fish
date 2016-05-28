21ae2f03b095ddf861333deaaf96bd043b708b10
Diéssica Gurskas <diessicode@gmail.com>
2016-05-28 10:52:56 -0300

    Update functions.fish

--- fish/functions.fish
+++ fish/functions.fish
@@ -1,3 +1,4 @@
+# Enter the directory and show its content
 function cd() {
   builtin cd $@ && ls
 }
@@ -5,4 +6,9 @@ function cd() {
 # Create a new directory and enter it
 function mkd() {
   mkdir -p "$@" && cd "$@"
+}
+
+# nvm with Fish shell's Bass (https://github.com/edc/bass#nvm)
+function nvm() {
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 }
