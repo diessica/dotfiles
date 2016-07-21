bef83ca93471de2b5166ca2a859ab86b1741bf19
Diéssica Gurskas <diessicode@gmail.com>
2016-07-21 20:42:24 -0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,8 @@
 set -e fish_greeting
 
 # Universal
-set -x PATH $HOME/.local/bin $PATH
 set -x EDITOR subl
-set -x VISUAL $EDITOR
+set -x PATH bin $PATH
 
 . "$HOME/.config/fish/functions/custom/export.fish"
 . "$HOME/.config/fish/functions/custom/aliases.fish"
