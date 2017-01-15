961c1aebd0fea3affafc0c324aded4a28cd4605f
Diéssica Gurskas <diessicode@gmail.com>
2017-01-15 19:40:19 -0200

    Remove export file from fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,9 @@
 set -e fish_greeting
 
 # Universal
-set -x EDITOR subl
+set -x EDITOR code
 set -x PATH bin $PATH
 
-. "$HOME/.config/fish/functions/custom/export.fish"
 . "$HOME/.config/fish/functions/custom/aliases.fish"
 . "$HOME/.config/fish/functions/custom/functions.fish"
+
