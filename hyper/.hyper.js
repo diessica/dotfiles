adda9a08f4437f3dc62a17bf8485e34b8c18d646
Diéssica Gurskas <diessicode@gmail.com>
2017-02-05 15:49:52 -0200

    Update Hyper configuration

---
+++ hyper/.hyper.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    fontSize: 12,
+    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    padding: '12px 14px',
+  },
+
+  plugins: [
+    'hyperterm-atom-dark',
+    'hyper-tabs-enhanced',
+    'hyper-blink'
+  ],
+};
