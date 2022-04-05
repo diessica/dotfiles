4477815992396d8079611ebb19293b99f201a698
Diessica Gurskas <diessica+github@pm.me>
2022-04-05 15:35:14 +0200

    m1 updates and other improvs

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,10 @@ set -e fish_greeting
 
 # Universal
 set -x EDITOR code
-set -x PATH bin $PATH
+
+fish_add_path /opt/homebrew/bin
 
 . "$HOME/.config/fish/functions/custom/aliases.fish"
 . "$HOME/.config/fish/functions/custom/functions.fish"
 
+starship init fish | source
